d23fc0da11e286e03e824d6c013aef69410eaac0
jaspering21 <jasperingclearlove@gmail.com>
2019-01-23 18:04:25 -0400

    Initial commit

---
+++ webservices/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.conf.urls import url
+from django.urls import include,path
+from rest_framework.urlpatterns import format_suffix_patterns
+from companies import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'stocks/', views.StockList.as_view()),
+]
+urlpatterns = format_suffix_patterns(urlpatterns)
+
