7288c04ee66200ad3c1ff048242ba296ccd99d55
jaspering21 <jasperingclearlove@gmail.com>
2019-01-25 17:54:14 -0400

    Update setting

--- webservices/settings.py
+++ webservices/settings.py
@@ -131,3 +131,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
